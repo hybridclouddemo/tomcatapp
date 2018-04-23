feba01310469fbf75bef8e302824d14d88151bab
HCP Demo User <hybridclouddemo@gmail.com>
2018-04-23 16:35:26 +0300

    Initial commit

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Enumeration;
+
+public class HelloServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException
+    {
+    	
+    	resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("Tomcat app v4\n\n");
+		out.println("Headers:<hr/>");
+		Enumeration<String> headerNames = req.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			out.print("Header Name: <em>" + headerName);
+			String headerValue = req.getHeader(headerName);
+			out.print("</em>, Header Value: <em>" + headerValue);
+			out.println("</em><br/>");
+		}
+    }
+}
